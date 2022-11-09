22c366b5074fa532d7bb85474d6eca7cef477d8c
YunByungil <dbsquddlfz@naver.com>
2022-11-09 20:13:57 +0900

    2주차 과제 및 리드미 작성 완료

---
+++ src/test/java/baseball/ApplicationTest.java
@@ -0,0 +1,95 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ApplicationTest extends NsTest {
+    @Test
+    void 게임종료_후_재시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "1", "597", "589", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("1~9숫자가 아닌 경우")
+    void 예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("120"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("숫자 말고 다른 문자를 넣은 경우")
+    void 예외_테스트3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("23b"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                );
+    }
+
+    @Test
+    @DisplayName("길이가 3이 아닌 경우 (length 2)")
+    void 예외_테스트4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                );
+    }
+
+    @Test
+    @DisplayName("길이가 3이 아닌 경우 (length 1)")
+    void 예외_테스트5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("중복된 숫자가 들어간 경우")
+    void 예외_테스트6() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("121"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("게임 재시작할 때 1or2가 아닐 때 예외")
+    void 예외_테스트7() {
+        //given
+        RestartGameException restartGameException = new RestartGameException();
+        String missType = "asdasd ";
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> {
+            restartGameException.isRestartNumber(missType);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
